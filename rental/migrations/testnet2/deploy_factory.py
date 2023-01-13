43714cf9b72a188dddf6f16946c7fdffdeb5cf99
0xKarasu <0xKarasu@protonmail.com>
2023-01-13 11:12:06 +0100

    :poop: migration file doesnt work

--- rental/migrations/testnet2/deploy_factory.py
+++ rental/migrations/testnet2/deploy_factory.py
@@ -14,14 +14,31 @@ selector = get_selector_from_name("initializer")
 ACCOUNT = os.getenv('OWNER_ADDRESS')
 print(selector)
 owner_address=0x625029eba3d6fcdad88e2cc0af513a019275c11e1c46c43460f4db9cdf2cb45
-implementation=0x00ad184ddc9d7ac7cc53833be5b9b56550ac330dabf1d114d41379911607be1c
-rental_hash=0x0402637b07042318060876540a157e8b9adf02c6503c9d0cdb2a65487051a06e
+implementation=0x3afe01ed713377a0c0a4200f44c7431aa7f4790e20d29826e43ee58b1990134
+rental_hash=0x1ff06790b799add7d3c89bfc51b77fb2ad3861f98a06af79cd1f36c1121920b
+proxy_class_hash=0xeafb0413e759430def79539db681f8a4eb98cf4196fe457077d694c6aeeb82
+
 calldata_len=3
-calldata=f"{ACCOUNT} {ACCOUNT} {rental_hash}"
+calldata=f"{ACCOUNT} {ACCOUNT} {rental_hash} {proxy_class_hash}"
 # Rental constructor parameters are
 #  proxy_admin: felt, _owner: felt, _rental_class_hash: felt
-PK = 'px.txt'
-INPUTS= f"implementation_hash={implementation} selector={selector} calldata_len={calldata_len} calldata={calldata}"
+PK = '../../px.txt'
+INPUTS= f"{implementation} {selector} {calldata_len} {calldata}"
+
+
+###### TESTNET 1 ########
+
+# print("BUILD")
+# run_command("./protostar build")
+# print("TEST")
+# run_command("./protostar test")
+# print("DECLARE")
+# out = run_command(f"protostar --profile testnet1 declare ./build/proxy.json --account-address {ACCOUNT} --private-key-path {PK} --json --max-fee auto")
+print("DEPLOY")
+out = run_command(f"protostar --profile testnet1 deploy {proxy_class_hash} --inputs {INPUTS} --account-address {ACCOUNT} --private-key-path './pk.txt' --json --max-fee auto")
+
+
+###### TESTNET 2 ########
 
 # print("BUILD")
 # run_command("./protostar build")
@@ -29,9 +46,8 @@ INPUTS= f"implementation_hash={implementation} selector={selector} calldata_len=
 # run_command("./protostar test")
 # print("DECLARE")
 # out = run_command(f"protostar --profile testnet2 declare ./build/proxy.json --account-address {ACCOUNT} --private-key-path {PK} --json --max-fee auto")
-print("DEPLOY")
-proxy_class_hash=0xeafb0413e759430def79539db681f8a4eb98cf4196fe457077d694c6aeeb82
-out = run_command(f"protostar --profile testnet2 deploy {proxy_class_hash} --inputs {INPUTS} --account-address {ACCOUNT} --private-key-path {PK} --json --max-fee auto")
+# print("DEPLOY")
+# out = run_command(f"protostar --profile testnet2 deploy {proxy_class_hash} --inputs {INPUTS} --account-address {ACCOUNT} --private-key-path {PK} --json --max-fee auto")
 
 print(out)
 print("DONE")
